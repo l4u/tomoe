0381238f0ae4f5808220441931841033c1cfb1cc
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2005-03-06 08:37:21 +0000

    update.

--- tomoe/trunk/test/testtomoe.c
+++ tomoe/trunk/test/testtomoe.c
@@ -1,9 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "tomoe.h"
 
 int
 main (int argc, char **argv)
 {
+  glyph *test_glyph = NULL;
+  candidates *matched = NULL;
+  int i, candidate_num = 0;
+
   tomoe_init ();
+
+  test_glyph = (glyph *) calloc (1, sizeof(glyph));
+  candidate_num = tomoe_get_matched (test_glyph, &matched);
+ 
+  if (candidate_num != 0)
+  {
+    if (!matched)
+    {
+      fprintf (stderr, "Candidate list is NULL!");
+      goto END;
+    }
+    if (candidate_num != matched->candidate_num)
+    {
+      fprintf (stderr, "The number of candidates does not equals!");
+      goto END;
+    }
+    for (i = 0; i < candidate_num; i++)
+    {
+      fprintf (stdout, "%s\t", matched->candidates[i].letter);
+    }
+  }
+  
+END:
+  free (test_glyph);
+
+  if (matched)
+  {
+    tomoe_free_matched (matched);
+  }
 
   tomoe_term ();
   return 0;
