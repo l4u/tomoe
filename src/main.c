d0c834185bc5b6a1ae3ff5bdb3bc4ee9898a4075
makeinu <makeinu@f1461e26-b952-4341-92c6-cb254f67b5cd>
2005-11-02 07:58:27 +0000

    Remove needless code.

--- src/main.c
+++ src/main.c
@@ -26,13 +26,13 @@
 #include <string.h>
 #include "tomoe.h"
 
-static tomoe_glyph * read_test_data ();
+static tomoe_glyph * read_glyph (void);
 
 #define LINE_BUF_SIZE 4096
 static char line_buf[LINE_BUF_SIZE];
 
 static tomoe_glyph *
-read_test_data ()
+read_glyph (void)
 {
     char *p = NULL;
     int stroke_num = 0;
@@ -42,15 +42,15 @@ read_test_data ()
     tomoe_stroke *strk = NULL;
     tomoe_point *pnt = NULL;
     FILE *fp = stdin;
-    tomoe_glyph *test_glyph = NULL;
+    tomoe_glyph *glyph = NULL;
 
-    test_glyph = calloc (1, sizeof (tomoe_glyph));
+    glyph = calloc (1, sizeof (tomoe_glyph));
 
     while ((p = fgets(line_buf, LINE_BUF_SIZE, fp)) != NULL)
     {
         if (strstr(p, "EOF") != NULL)
         {
-            tomoe_glyph_free (test_glyph);
+            tomoe_glyph_free (glyph);
             return NULL;
         }
 
@@ -59,9 +59,9 @@ read_test_data ()
             continue;
         }
         sscanf(p + 1, "%d", &stroke_num);
-        test_glyph->stroke_num = stroke_num;
+        glyph->stroke_num = stroke_num;
         strk = calloc (stroke_num, sizeof (tomoe_stroke));
-        test_glyph->strokes = strk;
+        glyph->strokes = strk;
 
         for (j = 0; j < stroke_num; j++)
         {
@@ -82,89 +82,33 @@ read_test_data ()
             }
         }
 
-        test_glyph->strokes = strk;
-        test_glyph->stroke_num = stroke_num;
-        return test_glyph;
+        glyph->strokes = strk;
+        glyph->stroke_num = stroke_num;
+        return glyph;
     }
-    tomoe_glyph_free (test_glyph);
+    tomoe_glyph_free (glyph);
     return NULL;
 }
-
-#if 0
-static glyph * 
-get_stroke ()
-{
-    char *p = NULL;
-    int stroke_num = 0;
-    int point_num = 0;
-    int j = 0;
-    int k = 0;
-    tomoe_stroke *strk = NULL;
-    tomoe_point *pnt = NULL;
-    FILE *fp = stdin;
-    glyph *test_glyph = NULL;
-
-    test_glyph = calloc (1, sizeof(tomoe_glyph));
-    /* printf("------------------ get_stroke() ----------------------\n"); */
-    while ((p = fgets(line_buf, LINE_BUF_SIZE, fp)) != NULL)
-    {
-        if (strstr(p, "EOF") != NULL)
-        {
-            return NULL;
-        }
-
-        if (p[0] != ':')
-        {
-            continue;
-        }
-        sscanf(p + 1, "%d", &stroke_num);
-        test_glyph->stroke_num = stroke_num;
-        /* printf("input_stroke_num=%d\n", stroke_num); */
-        strk = calloc (stroke_num, sizeof (tomoe_stroke));
-        test_glyph->strokes = strk;
-        /* letter_alloc_contents(&input, stroke_num); */
-
-        for (j = 0; j < stroke_num; j++)
-        {
-            p = fgets(line_buf, LINE_BUF_SIZE, fp);
-            sscanf(p, "%d", &point_num);
-            p = strchr(p, ' ');
-
-            strk[j].point_num = point_num;
-            pnt = calloc (point_num, sizeof (tomoe_point));
-            strk[j].points = pnt;
-            for (k = 0; k < point_num; k++)
-            {
-                int x, y;
-                sscanf(p, " (%d %d)", &x, &y);
-                p = strchr(p, ')') + 1;
-                pnt[k].x = x;
-                pnt[k].y = y;
-            }
-        }
-        return test_glyph;
-    }
-    return NULL;
-}
-#endif
 
 int
 main (int argc, char **argv)
 {
-    tomoe_glyph *test_glyph = NULL;
+    tomoe_glyph *glyph = NULL;
     tomoe_candidate **matched = NULL;
     int i, candidate_num = 0;
+
+    /* FIXME! read arguments */
 
     tomoe_init ();
 
     while (1)
     {
-        test_glyph = read_test_data ();
+        glyph = read_glyph ();
 
-        if (!test_glyph)
+        if (!glyph)
             break;
 
-        candidate_num = tomoe_get_matched (test_glyph, &matched);
+        candidate_num = tomoe_get_matched (glyph, &matched);
 
         if (candidate_num != 0)
         {
@@ -180,7 +124,7 @@ main (int argc, char **argv)
             }
         }
 
-        tomoe_glyph_free (test_glyph);
+        tomoe_glyph_free (glyph);
   
         if (matched)
         {
