9053d0b8caef460d597f51c32cd913e2b066287e
makeinu <makeinu@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-01-12 06:09:27 +0000

    Fix clash bug.

--- lib/tomoe-dict.c
+++ lib/tomoe-dict.c
@@ -145,8 +145,7 @@ tomoe_dict_new (const char *filename)
 
     dict = calloc (1, sizeof (tomoe_dict));
     dict->letter_num = DICT_LETTER_INITIAL_SIZE;
-    dict->letters    = calloc (letter_num,
-                               sizeof (DICT_LETTER_INITIAL_SIZE));
+    dict->letters    = calloc (dict->letter_num, sizeof (tomoe_letter));
 
     while ((p = fgets (line_buf, LINE_BUF_SIZE, fp)) != NULL)
     {
