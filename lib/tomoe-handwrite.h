756f5d78242c40d5da790991d43714da15d367dc
makeinu <makeinu@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-01-12 08:51:10 +0000

    Implement some functions.

--- lib/tomoe-handwrite.h
+++ lib/tomoe-handwrite.h
@@ -34,17 +34,63 @@ extern "C" {
 #include "tomoe-data-types.h"
 #include "tomoe-dict.h"
 
+/**
+ * @typedef typedef struct _tomoe_hw_context tomoe_hw_context;
+ *
+ * A struct type which is used for keeping a context of handwriting. Typically
+ * one handwrite canvas will have one tomoe_hw_context.
+ */
 typedef struct _tomoe_hw_context tomoe_hw_context;
 
+/**
+ * @brief Create a tomoe_hw_context struct.
+ * @return - Pointer to the newly allocated tomoe_hw_context struct.
+ */
 tomoe_hw_context   *tomoe_hw_context_new        (void);
+
+/**
+ * @brief Free an allocated tomoe_hw_context struct.
+ * @param ctx - Pointer to the tomoe_hw_context to free.
+ */
 void                tomoe_hw_context_free       (tomoe_hw_context *ctx);
 
+/**
+ * @brief Append a tomoe dictionary into a tomoe_hw_context.
+ * @param ctx  - The tomoe_hw_context to remove a dictionary.
+ * @param dict - A tomoe dictionary to append. NULL pointer will be ignored.
+ */
 void                tomoe_hw_append_dictionary  (tomoe_hw_context *ctx,
                                                  tomoe_dict       *dict);
+
+/**
+ * @brief Remove a tomoe dictionary from a tomoe_hw_context.
+ * @param ctx  - The tomoe_hw_context to append a dictionary.
+ * @param dict - A otmoe dictionary to remove. NULL pointer will be ignored.
+ */
 void                tomoe_hw_remove_dictionary  (tomoe_hw_context *ctx,
                                                  tomoe_dict       *dict);
+
+/**
+ * @brief Return number of dictionaries which is kept in a tomoe_hw_context.
+ * @param ctx - Pointer to the tomoe_hw_context which keeps tomoe dictionaries.
+ * @return    - Number of dictionaries.
+ */
+unsigned int        tomoe_hw_get_number_of_dictionaries
+                                                (tomoe_hw_context *ctx);
+
+/**
+ * @brief Return array of tomoe_dict poitners which is kept by a
+ *        tomoe_hw_context.
+ * @param ctx - Pointer to the tomoe_hw_context which keeps tomoe dictionaries.
+ * @return    - An array of tomoe_dict pointers. The array is terminated by NULL
+ *              pointer. NULL will be returned if the context has no dictionary.
+ */
 const tomoe_dict  **tomoe_hw_get_dictionaries   (tomoe_hw_context *ctx);
 
+#if 0
+/*
+ * Not implemented yet.
+ */
 void                tomoe_hw_set_canvas_width   (tomoe_hw_context *ctx,
                                                  unsigned int      width);
 void                tomoe_hw_set_canvas_height  (tomoe_hw_context *ctx,
@@ -63,6 +109,7 @@ const tomoe_glyph  *tomoe_hw_get_glyph          (tomoe_hw_context *ctx);
 
 const tomoe_candidate
                   **tomoe_hw_get_candidates     (tomoe_hw_context *ctx);
+#endif
 
 #ifdef	__cplusplus
 }
