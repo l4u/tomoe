a10e4bc7c249c221f8441416788be44f24457f59
makeinu <makeinu@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-11-16 02:49:44 +0000

    Fix C++ style comments.

--- src/main.c
+++ src/main.c
@@ -126,10 +126,12 @@ main (int argc, char **argv)
 
             for (i = 0; i < candidate_num; i++)
             {
-                //const tomoe_candidate* p = (const tomoe_candidate*)tomoe_array_getConst (matched, i);
-                //if (i > 0)
-                //    fprintf (stdout, " ");
-                //fprintf (stdout, " %s", p->character->letter);
+#if 0
+                const tomoe_candidate* p = (const tomoe_candidate*)tomoe_array_getConst (matched, i);
+                if (i > 0)
+                    fprintf (stdout, " ");
+                fprintf (stdout, " %s", p->character->letter);
+#endif
             }
             fprintf (stdout, "\n");
         }
