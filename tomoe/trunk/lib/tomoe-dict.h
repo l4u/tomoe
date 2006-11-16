85c3aeaaefa6de917dc79e28c7fd3a7b18d5c207
makeinu <makeinu@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-11-16 04:11:47 +0000

    Fix indent.

--- tomoe/trunk/lib/tomoe-dict.h
+++ tomoe/trunk/lib/tomoe-dict.h
@@ -48,27 +48,27 @@ typedef struct _tomoe_dict tomoe_dict;
  * @param editable - Editability of the dictionary.
  * @return Pointer to newly allocated tomoe_dict object.
  */
-tomoe_dict  *tomoe_dict_new               (const char    *filename,
-                                           tomoe_bool     editable);
+tomoe_dict*     tomoe_dict_new                  (const char    *filename,
+                                                 tomoe_bool     editable);
 
 /**
  * @brief Increase reference count.
  * @param t_dict - Pointer to the tomoe_dict object to increase reference count.
  * @return The tomoe_dict.
  */
-tomoe_dict*  tomoe_dict_add_ref           (tomoe_dict*    t_dict);
+tomoe_dict*     tomoe_dict_add_ref              (tomoe_dict*    t_dict);
 
 /**
  * @brief Decrease reference count and free if zero.
  * @param t_dict - Pointer to the tomoe_dict object to free.
  */
-void         tomoe_dict_free              (tomoe_dict*    t_dict);
+void            tomoe_dict_free                 (tomoe_dict*    t_dict);
 
 /**
  * @brief Save tomoe dictionary.
  * @param t_dict - Pointer to the tomoe_dict object.
  */
-void         tomoe_dict_save              (tomoe_dict*    t_dict);
+void            tomoe_dict_save                 (tomoe_dict*    t_dict);
 
 /**
  * @brief Get the file name of the tomoe dictionary.
@@ -77,32 +77,32 @@ void         tomoe_dict_save              (tomoe_dict*    t_dict);
  *         wasn't load from a file, or newly allocated in a program and it
  *         doesn't saved yet.
  */
-const char*  tomoe_dict_get_filename      (tomoe_dict    *t_dict);
+const char*     tomoe_dict_get_filename         (tomoe_dict    *t_dict);
 
 /**
  * @brief Get the dictionary name.
  * @param t_dict - Pointer to the tomoe_dict object.
  * @return Name of the dictionary.
  */
-const char*  tomoe_dict_get_name          (tomoe_dict*    t_dict);
+const char*     tomoe_dict_get_name             (tomoe_dict*    t_dict);
 
 /**
  * @brief Get editable property.
  * @param t_dict - Pointer to the tomoe_dict object.
  * @return 1 - is editable, 0 - is not editable
  */
-tomoe_bool   tomoe_dict_get_editable      (tomoe_dict*    t_dict);
+tomoe_bool      tomoe_dict_get_editable         (tomoe_dict*    t_dict);
 
-tomoe_bool   tomoe_dict_get_modified      (tomoe_dict    *dict);
-void         tomoe_dict_set_modified      (tomoe_dict    *dict,
-                                           tomoe_bool     modified);
+tomoe_bool      tomoe_dict_get_modified         (tomoe_dict    *dict);
+void            tomoe_dict_set_modified         (tomoe_dict    *dict,
+                                                 tomoe_bool     modified);
 
 /**
  * @brief Get character count
  * @param t_dict - Pointer to the tomoe_dict object.
  * @return Count of the characters.
  */
-int          tomoe_dict_get_size          (tomoe_dict*    t_dict);
+int             tomoe_dict_get_size             (tomoe_dict*    t_dict);
 
 /* Editable methods */
 
@@ -111,28 +111,28 @@ int          tomoe_dict_get_size          (tomoe_dict*    t_dict);
  * @param t_dict - Pointer to the tomoe_dict object.
  * @param add  - Pointer to the tomoe_char object.
  */
-void         tomoe_dict_add_char          (tomoe_dict*    t_dict,
-                                           tomoe_char*    remove);
+void            tomoe_dict_add_char             (tomoe_dict*    t_dict,
+                                                 tomoe_char*    remove);
 
-void         tomoe_dict_insert            (tomoe_dict    *dict,
-                                           int            position,
-                                           tomoe_char    *insert);
+void            tomoe_dict_insert               (tomoe_dict    *dict,
+                                                 int            position,
+                                                 tomoe_char    *insert);
 
 /**
  * @brief Remove a character by a character object
  * @param t_dict   - Pointer to the tomoe_dict object.
  * @param remove - Pointer to the tomoe_char object.
  */
-void         tomoe_dict_remove_by_char    (tomoe_dict*    t_dict,
-                                           tomoe_char*    remove);
+void            tomoe_dict_remove_by_char       (tomoe_dict*    t_dict,
+                                                 tomoe_char*    remove);
 
 /**
  * @brief Remove a character by index
  * @param t_dict   - Pointer to the tomoe_dict object.
  * @param remove - Index of the character.
  */
-void         tomoe_dict_remove_by_index   (tomoe_dict*    t_dict,
-                                           int            remove);
+void            tomoe_dict_remove_by_index      (tomoe_dict*    t_dict,
+                                                 int            remove);
 
 /**
  * @brief Find a character
@@ -140,8 +140,8 @@ void         tomoe_dict_remove_by_index   (tomoe_dict*    t_dict,
  * @param find - Pointer to the tomoe_char object.
  * @return Index of the character.
  */
-int          tomoe_dict_find_index        (tomoe_dict*    t_dict,
-                                           tomoe_char*    find);
+int             tomoe_dict_find_index           (tomoe_dict*    t_dict,
+                                                 tomoe_char*    find);
 
 /**
  * @brief Get a character by index
@@ -149,15 +149,15 @@ int          tomoe_dict_find_index        (tomoe_dict*    t_dict,
  * @param index - Index of the character.
  * @return tomoe_char object.
  */
-tomoe_char*  tomoe_dict_char_by_index     (tomoe_dict*    t_dict,
-                                           int index);
+tomoe_char*     tomoe_dict_char_by_index        (tomoe_dict*    t_dict,
+                                                 int            index);
 
 /**
  * @brief Return an array of tomoe_char which is kept in a tomoe_dict.
  * @param t_dict - Pointer to the tomoe_dict struct.
  * @return The array of tomoe_char.
  */
-tomoe_array* tomoe_dict_get_letters       (tomoe_dict    *t_dict);
+tomoe_array*    tomoe_dict_get_letters          (tomoe_dict    *t_dict);
 
 /* search methods */
 
@@ -167,8 +167,8 @@ tomoe_array* tomoe_dict_get_letters       (tomoe_dict    *t_dict);
  * @param input  - Pointer to tomoe_glyph matchkey.
  * @return The array of tomoe_candidate.
  */
-tomoe_array* tomoe_dict_search_by_strokes (const tomoe_dict* t_dict,
-                                           tomoe_glyph*      input);
+tomoe_array*    tomoe_dict_search_by_strokes    (const tomoe_dict* t_dict,
+                                                 tomoe_glyph*      input);
 
 /**
  * @brief Match reading of tomoe_char with input.
@@ -176,11 +176,11 @@ tomoe_array* tomoe_dict_search_by_strokes (const tomoe_dict* t_dict,
  * @param input  - Pointer to string matchkey
  * @return The array of tomoe_candidate.
  */
-tomoe_array* tomoe_dict_search_by_reading (const tomoe_dict* t_dict,
-                                           const char*       input);
+tomoe_array*    tomoe_dict_search_by_reading    (const tomoe_dict* t_dict,
+                                                 const char*       input);
 
 #ifdef TOMOE_DICT__USE_XSL_METHODS
-xsltStylesheetPtr  tomoe_dict_get_meta_xsl(tomoe_dict*       t_dict);
+xsltStylesheetPtr  tomoe_dict_get_meta_xsl      (tomoe_dict*       t_dict);
 #endif
 
 #ifdef	__cplusplus
