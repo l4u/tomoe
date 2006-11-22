7c5ada6f082a8803146f9b9a17f34b7a612f5b28
kous <kous@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-11-22 07:18:11 +0000

    * test/: moved test data for searching by stroke to test/data/. * test/context_spec.rb: used test/data/*.data.

--- tomoe/trunk/test/context_spec.rb
+++ tomoe/trunk/test/context_spec.rb
@@ -10,28 +10,37 @@ context "Tomoe::Context" do
     @context.load_config(@config_file.path)
   end
 
-  specify "Search by strokes" do
-    writing = Tomoe::Writing.new
-    strokes = [
-             [[51, 29], [177, 41]],
-             [[99, 65], [219, 77]],
-             [[27, 131], [261, 131]],
-             [[129, 17], [57, 203]],
-             [[111, 71], [219, 173]],
-             [[81, 161], [93, 281]],
-             [[99, 167], [207, 167], [189, 245]],
-             [[99, 227], [189, 227]],
-             [[111, 257], [189, 245]],
-            ]
-    strokes.each do |first_point, *rest_points|
-      writing.move_to(*first_point)
-      rest_points.each do |x, y|
-        writing.line_to(x, y)
-      end
-    end
-    cands = @context.search_by_strokes(writing)
+  def numbers_to_point(str)
+    point = str.split.collect {|x| Integer(x)}
+    raise ArgumentError if point.size != 2
+    point
+  end
 
-    cands.collect {|cand| cand.character.code}.should == ["春", "屠"]
+  specify "Search by strokes" do
+    Dir.glob(File.join(@test_data_dir, "*.data")).each do |file|
+      base = File.basename(file)
+      expected = [base]
+      writing = Tomoe::Writing.new
+      File.open(file) do |f|
+        expected << f.gets.split
+        f.each do |line|
+          next if /\A\s*\z/ =~ line
+          begin
+            first_point, *rest_points = line.split(/,/)
+            numbered_first_point = numbers_to_point(first_point)
+            writing.move_to(*numbered_first_point)
+            rest_points.each do |point|
+              writing.line_to(*numbers_to_point(point))
+            end
+          rescue ArgumentError
+            raise "invalid format in #{file} at #{f.lineno}: #{line}"
+          end
+        end
+      end
+      cands = @context.search_by_strokes(writing)
+
+      [base, cands.collect {|cand| cand.character.code}].should == expected
+    end
   end
 
   specify "Search by reading" do
