2a169b6d9dc601b2254a53e4fee6e574dde59383
kous <kous@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-12-05 09:01:29 +0000

    * module/dict/tomoe-dict-ptr-array.[ch]: added. * module/dict/Makefile.am, module/dict/tomoe-dict-unihan.c,   module/dict/tomoe-dict-xml.c: used tomoe-dict-ptr-array.[ch].

---
+++ tomoe/trunk/module/dict/tomoe-dict-ptr-array.c
@@ -0,0 +1,186 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2006 Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA  02111-1307  USA
+ *
+ *  $Id$
+ */
+
+#include <string.h>
+
+#include "tomoe-dict-ptr-array.h"
+
+#include <tomoe-candidate.h>
+#include <glib-utils.h>
+
+typedef struct _TomoeDictSearchContext {
+    TomoeQuery *query;
+    GList *results;
+} TomoeDictSearchContext;
+
+static gint
+letter_compare_func (gconstpointer a, gconstpointer b)
+{
+    TomoeChar *ca = *(TomoeChar **) a;
+    TomoeChar *cb = *(TomoeChar **) b;
+    return tomoe_char_compare (ca, cb);
+}
+
+void
+_tomoe_dict_ptr_array_sort (GPtrArray *chars)
+{
+    g_ptr_array_sort (chars, letter_compare_func);
+}
+
+gboolean
+_tomoe_dict_ptr_array_register_char (GPtrArray *chars, TomoeChar *chr)
+{
+    g_ptr_array_add (chars, g_object_ref (G_OBJECT (chr)));
+    _tomoe_dict_ptr_array_sort (chars);
+
+    return TRUE;
+}
+
+gboolean
+_tomoe_dict_ptr_array_unregister_char (GPtrArray *chars, const gchar *utf8)
+{
+    TomoeChar *removed = NULL;
+    gint i, len, index = -1;
+
+    g_return_val_if_fail (utf8 && *utf8 != '\0', FALSE);
+
+    len = chars->len;
+    for (i = 0; i < len; i++) {
+        TomoeChar *chr = g_ptr_array_index (chars, i);
+        if (g_str_equal (tomoe_char_get_utf8(chr), utf8)) {
+            index = i;
+            removed = chr;
+            break;
+        }
+    }
+
+    if (index >= 0) {
+        g_ptr_array_remove_index (chars, index);
+        g_object_unref (removed);
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+TomoeChar *
+_tomoe_dict_ptr_array_get_char (GPtrArray *chars, const gchar *utf8)
+{
+    gint i, len;
+
+    g_return_val_if_fail (utf8 && *utf8 != '\0', NULL);
+
+    len = chars->len;
+    for (i = 0; i < len; i++) {
+        TomoeChar *chr = g_ptr_array_index (chars, i);
+        if (g_str_equal (tomoe_char_get_utf8(chr), utf8)) {
+            return chr;
+        }
+    }
+
+    return NULL;
+}
+
+static gboolean
+does_match_char_with_n_strokes (TomoeChar *chr, gint min, gint max)
+{
+    TomoeWriting *writing;
+    gint n_strokes;
+
+    if (min < 0 && max < 0)
+        return TRUE;
+
+    writing = tomoe_char_get_writing (chr);
+    if (!writing)
+        return FALSE;
+
+    n_strokes = tomoe_writing_get_n_strokes (writing);
+    return ((min < 0 || min <= n_strokes) &&
+            (max < 0 || max >= n_strokes));
+}
+
+static gint
+compare_reading (gconstpointer a, gconstpointer b)
+{
+    TomoeReading *reading, *searched_reading;
+
+    reading = TOMOE_READING(a);
+    searched_reading = TOMOE_READING(b);
+    return strcmp(tomoe_reading_get_reading(reading),
+                  tomoe_reading_get_reading(searched_reading));
+}
+
+static gboolean
+does_match_char_with_readings (TomoeChar *chr, TomoeReading *reading)
+{
+    if (!reading)
+        return TRUE;
+
+    if (g_list_find_custom ((GList *)tomoe_char_get_readings (chr),
+                            reading, compare_reading))
+        return TRUE;
+    else
+        return FALSE;
+}
+
+static void
+collect_chars_by_query (gpointer data, gpointer user_data)
+{
+    TomoeChar *chr = data;
+    TomoeDictSearchContext *context = user_data;
+    TomoeQuery *query;
+    TomoeReading *reading;
+    gint min_n_strokes, max_n_strokes;
+
+    query = context->query;
+
+    min_n_strokes = tomoe_query_get_min_n_strokes (query);
+    max_n_strokes = tomoe_query_get_max_n_strokes (query);
+    if (!does_match_char_with_n_strokes (chr, min_n_strokes, max_n_strokes))
+        return;
+
+    reading = g_list_nth_data ((GList *)tomoe_query_get_readings (query), 0);
+    if (!does_match_char_with_readings (chr, reading))
+        return;
+
+    context->results = g_list_prepend (context->results,
+                                       tomoe_candidate_new (chr));
+}
+
+GList *
+_tomoe_dict_ptr_array_search (GPtrArray *chars, TomoeQuery *query)
+{
+    TomoeDictSearchContext search_context;
+
+    search_context.query = g_object_ref (query);
+    search_context.results = NULL;
+
+    g_ptr_array_foreach_reverse (chars, collect_chars_by_query,
+                                 &search_context);
+    g_object_unref (search_context.query);
+
+    return search_context.results;
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab
+*/
