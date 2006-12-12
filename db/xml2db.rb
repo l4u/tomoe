4a798dea41c4b6ce5695a401e9a1a574132ab158
kous <kous@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-12-12 05:00:59 +0000

    * db/xml2db.rb: used Tomoe::DictMySQL#register. * test/tomoe-spec-utils.rb (TomoeSpecUtils::Config::db_config):   used ENV["TOMOE_ENV"] as default value.

--- db/xml2db.rb
+++ db/xml2db.rb
@@ -27,6 +27,9 @@ puts "done."
 dict = Tomoe::Dict.new("xml",
                        "filename" => TomoeSpecUtils::Config.dictionaries.first,
                        "editable" => false)
+config = TomoeSpecUtils::Config.db_config.merge("editable" => true)
+mysql_dict = Tomoe::Dict.new("mysql", config)
+
 cands = dict.search(Tomoe::Query.new)
 puts "dict size: #{cands.size}"
 
@@ -34,24 +37,7 @@ print "converting..."
 $stdout.flush
 $KCODE = "u"
 cands.each_with_index do |cand, i|
-  char = cand.char
-  Char.new(:utf8 => char.utf8,
-           :n_strokes => char.n_strokes < 0 ? nil : char.n_strokes,
-           :variant => char.variant).save!
-  char.readings.each do |reading|
-    Reading.new(:utf8 => char.utf8,
-                :reading_type => reading.type.to_i,
-                :reading => reading.reading).save!
-  end
-  char.radicals.each do |radical|
-    Radical.new(:utf8 => char.utf8,
-                :radical_utf8 => radical).save!
-  end
-  char.each do |key, value|
-    MetaDatum.new(:utf8 => char.utf8,
-                  :key => key,
-                  :value => value).save!
-  end
+  mysql_dict.register(cand.char)
   if (i % 1000).zero? and !i.zero?
     print "#{i}."
     $stdout.flush
