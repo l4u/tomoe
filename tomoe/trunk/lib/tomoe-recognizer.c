375353de40e105e571bdfe2780afe32051e445eb
kous <kous@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-12-14 08:34:39 +0000

    * lib/tomoe-dict.c: check TOMOE_DICT_MODULE_DIR   environemnet variable as default dictionary module directory. * lib/tomoe-recognizer.c: check TOMOE_RECOGNIZER_MODULE_DIR   environemnet variable as default recognizer module directory.

--- tomoe/trunk/lib/tomoe-recognizer.c
+++ tomoe/trunk/lib/tomoe-recognizer.c
@@ -32,6 +32,8 @@ void
 tomoe_recognizer_load (const gchar *base_dir)
 {
     if (!base_dir)
+        base_dir = g_getenv ("TOMOE_RECOGNIZER_MODULE_DIR");
+    if (!base_dir)
         base_dir = RECOGNIZER_MODULEDIR;
 
     recognizers = g_list_concat (tomoe_module_load_modules (base_dir),
