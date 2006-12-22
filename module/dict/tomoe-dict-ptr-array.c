b4725b2520ff464d24a05ea4b2f38b582686f591
kous <kous@f1461e26-b952-4341-92c6-cb254f67b5cd>
2006-12-22 05:40:41 +0000

    * module/dict/tomoe-dict-ptr-array.c: supported variant search and   utf8 search.

--- module/dict/tomoe-dict-ptr-array.c
+++ module/dict/tomoe-dict-ptr-array.c
@@ -101,6 +101,30 @@ _tomoe_dict_ptr_array_get_char (GPtrArray *chars, const gchar *utf8)
 }
 
 static gboolean
+does_match_char_with_utf8 (TomoeChar *chr, const gchar *utf8)
+{
+    if (!utf8)
+        return TRUE;
+
+    return g_utf8_collate (tomoe_char_get_utf8 (chr), utf8) == 0;
+}
+
+static gboolean
+does_match_char_with_variant (TomoeChar *chr, const gchar *variant)
+{
+    const gchar *chr_variant;
+
+    if (!variant)
+        return TRUE;
+
+    chr_variant = tomoe_char_get_variant (chr);
+    if (!chr_variant)
+        return FALSE;
+
+    return g_utf8_collate (chr_variant, variant) == 0;
+}
+
+static gboolean
 does_match_char_with_n_strokes (TomoeChar *chr, gint min, gint max)
 {
     TomoeWriting *writing;
@@ -191,23 +215,19 @@ collect_chars_by_query (gpointer data, gpointer user_data)
 {
     TomoeChar *chr = data;
     TomoeDictSearchContext *context = user_data;
-    TomoeQuery *query;
+    TomoeQuery *q;
 
-    query = context->query;
-
-    if (!does_match_char_with_n_strokes (chr,
-                                         tomoe_query_get_min_n_strokes (query),
-                                         tomoe_query_get_max_n_strokes (query)))
-        return;
-
-    if (!does_match_char_with_readings (chr, tomoe_query_get_readings (query)))
-        return;
-
-    if (!does_match_char_with_radicals (chr, tomoe_query_get_radicals (query)))
-        return;
-
-    context->results = g_list_prepend (context->results,
-                                       tomoe_candidate_new (chr));
+    q = context->query;
+    if (does_match_char_with_utf8 (chr, tomoe_query_get_utf8 (q)) &&
+        does_match_char_with_variant (chr, tomoe_query_get_variant (q)) &&
+        does_match_char_with_n_strokes (chr,
+                                        tomoe_query_get_min_n_strokes (q),
+                                        tomoe_query_get_max_n_strokes (q)) &&
+        does_match_char_with_readings (chr, tomoe_query_get_readings (q)) &&
+        does_match_char_with_radicals (chr, tomoe_query_get_radicals (q))) {
+        context->results = g_list_prepend (context->results,
+                                           tomoe_candidate_new (chr));
+    }
 }
 
 static void
