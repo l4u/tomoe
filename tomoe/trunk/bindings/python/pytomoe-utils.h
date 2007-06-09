584b5814bf6b85f088bffc0742ad76c5b649e47c
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-09 12:00:11 +0000

    * bindings/python/pytomoe-utils.h: define CONVERT_STRING_LIST. rename CONVERT_LIST to CONVERT_OBJECT_LIST. * bindings/python/*.override: Follow the above change. Now testRadical() works fine.

--- tomoe/trunk/bindings/python/pytomoe-utils.h
+++ tomoe/trunk/bindings/python/pytomoe-utils.h
@@ -1,13 +1,25 @@
 
-#define CONVERT_LIST(py_list, g_list)	\
-	{				\
-		GList *node;		\
-		py_list = PyList_New (0);	\
+#define CONVERT_OBJECT_LIST(py_list, g_list)						\
+	{										\
+		GList *node;								\
+		py_list = PyList_New (0);						\
 		for (node = g_list; node; node = g_list_next(node))			\
 		{									\
 			PyObject *py_obj = pygobject_new ((GObject *)node->data);	\
-			PyList_Append (py_list, py_obj);	\
-			Py_DECREF(py_obj);	\
-		}				\
+			PyList_Append (py_list, py_obj);				\
+			Py_DECREF(py_obj);						\
+		}									\
+	}
+
+#define CONVERT_STRING_LIST(py_list, g_list)							\
+	{											\
+		GList *node;									\
+		py_list = PyList_New (0);							\
+		for (node = g_list; node; node = g_list_next(node))				\
+		{										\
+			PyObject *py_obj = PyString_FromString ((const gchar *)node->data);	\
+			PyList_Append (py_list, py_obj);					\
+			Py_DECREF(py_obj);							\
+		}										\
 	}
 
