6a50a2120a2a5eb92a4593d597d2fe92c4331981
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-09 11:05:28 +0000

    * test/python/test_char.rb: More test code for TomoeChar. But testRadical() causes seg fault now. Fix it.

--- test/python/test_char.py
+++ test/python/test_char.py
@@ -13,29 +13,43 @@ class TomoeCharTest(unittest.TestCase):
 
         tomoe_char = tomoe.Char()
         self.assertEqual(tomoe_char.get_utf8(), None)
-
         tomoe_char.set_utf8(utf8)
         self.assertEqual(tomoe_char.get_utf8(), utf8)
 
+        tomoe_char = tomoe.Char(utf8 = utf8)
+        self.assertEqual(tomoe_char.get_utf8(), utf8)
+
+        new_utf8 = '地'
+        tomoe_char.set_utf8(new_utf8)
+        self.assertEqual(tomoe_char.get_utf8(), new_utf8)
+
     def testStrokes(self):
         n_strokes = 2
-
         tomoe_char = tomoe.Char()
         self.assertEqual(tomoe_char.get_n_strokes(), -1)
+        tomoe_char.set_n_strokes(n_strokes)
+        self.assertEqual(tomoe_char.get_n_strokes(), n_strokes)
 
+        tomoe_char = tomoe.Char(n_strokes = n_strokes)
+        self.assertEqual(tomoe_char.get_n_strokes(), n_strokes)
+
+        n_strokes = 3
         tomoe_char.set_n_strokes(n_strokes)
         self.assertEqual(tomoe_char.get_n_strokes(), n_strokes)
 
     def testReading(self):
         reading_string = 'いけ'
-        reading = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string)
+        reading1 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string)
 
         tomoe_char = tomoe.Char()
         self.assertEqual(len(tomoe_char.get_readings()), 0)
 
-        tomoe_char.add_reading(reading)
+        tomoe_char.add_reading(reading1)
+        self.assertEqual(tomoe_char.get_readings(), [reading1])
 
-        self.fail()
+        reading2 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string)
+        tomoe_char.add_reading(reading2)
+        self.assertEqual(tomoe_char.get_readings().sort(), [reading1, reading2].sort())
 
     def testRadical(self):
         radical_string = 'いけ'
@@ -45,17 +59,35 @@ class TomoeCharTest(unittest.TestCase):
 
         tomoe_char.add_radical(radical_string)
 
-        #self.assertNotEqual(len(tomoe_char.get_radicals()), 0)
-        self.fail()
+        self.assertNotEqual(len(tomoe_char.get_radicals()), 0)
 
     def testWriting(self):
-        self.fail()
+        writing = tomoe.Writing()
+
+        tomoe_char = tomoe.Char()
+        self.assertEqual(tomoe_char.get_writing(), None)
+        tomoe_char.set_writing(writing)
+        self.assertEqual(tomoe_char.get_writing(), writing)
+
+        tomoe_char = tomoe.Char(writing = writing)
+        self.assertEqual(tomoe_char.get_writing(), writing)
+
+        writing.move_to(1, 1)
+        writing.line_to(2, 2)
+        tomoe_char.set_writing(writing)
+        self.assertEqual(tomoe_char.get_writing(), writing)
 
     def testVariant(self):
         variant = '池'
         tomoe_char = tomoe.Char()
         self.assertEqual(tomoe_char.get_variant(), None)
+        tomoe_char.set_variant(variant)
+        self.assertEqual(tomoe_char.get_variant(), variant)
 
+        tomoe_char = tomoe.Char(variant = variant)
+        self.assertEqual(tomoe_char.get_variant(), variant)
+
+        variant = '地'
         tomoe_char.set_variant(variant)
         self.assertEqual(tomoe_char.get_variant(), variant)
 
