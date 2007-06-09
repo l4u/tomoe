d415b74fe4c3900f6ef3cc71de34f6e714f69811
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-09 00:29:49 +0000

    more TomoeReading test.

--- test/python/test_reading.py
+++ test/python/test_reading.py
@@ -38,36 +38,55 @@ class TomoeReadingTest(unittest.TestCase):
 
     def testCompare(self):
         reading_string = "いけ"
+        reading_string2 = "ほげ"
         reading1 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string)
         reading2 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string)
+        reading3 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string2)
         self.assertEqual(reading1.compare(reading2), 0)
+        self.assertNotEqual(reading1.compare(reading3), 0)
 
         reading1 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string)
         reading2 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string)
+        reading3 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string2)
+        reading4 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string2)
         self.assertEqual(reading1.compare(reading2), 0)
         self.assertEqual(reading2.compare(reading1), 0)
+        self.assertNotEqual(reading2.compare(reading3), 0)
+        self.assertNotEqual(reading1.compare(reading4), 0)
 
         reading1 = tomoe.Reading(tomoe.READING_JA_ON, reading_string)
         reading2 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string)
+        reading3 = tomoe.Reading(tomoe.READING_JA_ON, reading_string2)
+        reading4 = tomoe.Reading(tomoe.READING_UNKNOWN, reading_string2)
         self.assertEqual(reading1.compare(reading2), 0)
         self.assertEqual(reading2.compare(reading1), 0)
+        self.assertNotEqual(reading2.compare(reading3), 0)
+        self.assertNotEqual(reading1.compare(reading4), 0)
 
         reading1 = tomoe.Reading(tomoe.READING_JA_ON, reading_string)
         reading2 = tomoe.Reading(tomoe.READING_JA_ON, reading_string)
+        reading3 = tomoe.Reading(tomoe.READING_JA_ON, reading_string2)
         self.assertEqual(reading1.compare(reading2), 0)
+        self.assertNotEqual(reading1.compare(reading3), 0)
 
         reading1 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string)
         reading2 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string)
+        reading3 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string2)
         self.assertEqual(reading1.compare(reading2), 0)
+        self.assertNotEqual(reading1.compare(reading3), 0)
 
         reading1 = tomoe.Reading(tomoe.READING_JA_ON, reading_string)
         reading2 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string)
+        reading3 = tomoe.Reading(tomoe.READING_JA_ON, reading_string2)
+        reading4 = tomoe.Reading(tomoe.READING_JA_KUN, reading_string2)
         self.assertNotEqual(reading1.compare(reading2), 0)
         self.assertNotEqual(reading2.compare(reading1), 0)
+        self.assertNotEqual(reading2.compare(reading3), 0)
+        self.assertNotEqual(reading1.compare(reading4), 0)
 
 if __name__ == '__main__':
-    suite = unittest.makeSuite(TomoeReadingTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    #unittest.main()
+    #suite = unittest.makeSuite(TomoeReadingTest)
+    #unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
 
 # vi:ts=4:nowrap:ai:expandtab
