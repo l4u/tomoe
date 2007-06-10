7afad9f7fc13abb8ccdf972c93e2b07c64c7d7c3
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-10 23:50:15 +0000

    * test/python/runtests.py: Test Unihan dict. Use insert() for addition path for loading tomoe module instead of append() since building tomoe module is prior to system tomoe module. * test/python/test_dict_xml.py: Create test dictionary in setUp(). * test/python/test_dict.py: Follow the above change. * test/python/test_dict_unihan.py: Override some methods of TomoeDictTest since Unihan dictionary module does not have some functions.

--- test/python/runtests.py
+++ test/python/runtests.py
@@ -3,11 +3,11 @@ import glob
 import os
 import sys
 import unittest
-sys.path.append('../../bindings/python/.libs')
+sys.path.insert(0, '../../bindings/python/.libs')
 import tomoe
 tomoe.tomoe_dict_set_default_module_dir('../../module/dict/.libs')
 
-SKIP_FILES = ['runtests', 'test_dict', 'test_dict_est', 'test_dict_unihan']
+SKIP_FILES = ['runtests', 'test_dict', 'test_dict_est']
 
 dir = os.path.split(os.path.abspath(__file__))[0]
 os.chdir(dir)
@@ -24,7 +24,7 @@ loader = unittest.TestLoader()
 for name in gettestnames():
     suite.addTest(loader.loadTestsFromName(name))
 
-testRunner = unittest.TextTestRunner()
+testRunner = unittest.TextTestRunner(verbosity=1)
 testRunner.run(suite)
 
 # vi:ts=4:nowrap:ai:expandtab
