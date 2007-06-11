ce5ed690f9e87440324439c4144de284a89be7b3
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-11 04:57:33 +0000

    * test/python/test_dict_est.py: Remove the directory which stores est databse whenever each test exits * test/python/runtests.py: test_dict_est.py is now avairable.

--- tomoe/trunk/test/python/test_dict_est.py
+++ tomoe/trunk/test/python/test_dict_est.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF=8 -*-
 import os
 import sys
+import shutil
 import unittest
 import tomoe
 import test_dict
@@ -8,9 +9,9 @@ import test_dict
 class TomoeDictEstTest(test_dict.TomoeDictTest):
 
     def setUp(self):
-        self.dict_name = "test-est"
-        self.dict = tomoe.Dict("Est", name = self.dict_name, database = self.dict_name, editable = True)
-        
+        self.dict_name = "est-dict"
+        self.dict = tomoe.Dict('Est', name = self.dict_name, database = self.dict_name, editable = True)
+
         tomoe_char = tomoe.Char(utf8 = '池', n_strokes = 6)
         reading = tomoe.Reading(tomoe.READING_JA_KUN, 'いけ')
         tomoe_char.add_reading(reading)
@@ -18,14 +19,14 @@ class TomoeDictEstTest(test_dict.TomoeDictTest):
         tomoe_char.add_reading(reading)
         reading = tomoe.Reading(tomoe.READING_UNKNOWN, 'あんのうん')
         tomoe_char.add_reading(reading)
-        self.dict.register_char(tomoe_char)
 
+        self.dict.register_char(tomoe_char)
         self.dict.flush()
 
     def tearDown(self):
         self.dict.flush()
         self.dict = None
         if os.access(self.dict_name, os.F_OK):
-            os.unlink(self.dict_name)
+            shutil.rmtree(self.dict_name)
 
 # vi:ts=4:nowrap:ai:expandtab
