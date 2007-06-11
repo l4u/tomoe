f5db0180bb56cd9a5a1a57905ea798127aaa8793
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-11 00:59:28 +0000

    implement some test.

--- tomoe/trunk/test/python/test_dict_unihan.py
+++ tomoe/trunk/test/python/test_dict_unihan.py
@@ -28,12 +28,6 @@ class TomoeDictUnihanTest(test_dict.TomoeDictTest):
         """
         return
 
-    def testUnknownKunReadingSearch(self):
-        """
-        Unihan dicitionary has no unknown-reading character
-        """
-        return
-
     def testOnUnknownReadingSearch(self):
         """
         Unihan dicitionary has no unknown-reading character
@@ -45,5 +39,49 @@ class TomoeDictUnihanTest(test_dict.TomoeDictTest):
         Unihan dicitionary has no unknown-reading character
         """
         return
+
+    def testUnknownKunReadingSearch(self):
+        char_codes = [ '池', '垳', '汪', '溏', '潢' ]
+        reading = 'いけ'
+        tomoe_reading = tomoe.Reading(tomoe.READING_UNKNOWN, reading)
+        tomoe_query = tomoe.Query()
+        tomoe_query.add_reading(tomoe_reading)
+
+        candidates = self.dict.search(tomoe_query)
+        for char, candidate in zip(sorted(set(char_codes)), sorted(self.dict.search(tomoe_query))):
+            self.assertEqual(candidate.get_char().get_utf8(), char)
+
+    def testKunReadingSearch(self):
+        char_codes = [ '池', '垳', '汪', '溏', '潢' ]
+        reading = 'いけ'
+        tomoe_reading = tomoe.Reading(tomoe.READING_JA_KUN, reading)
+        tomoe_query = tomoe.Query()
+        tomoe_query.add_reading(tomoe_reading)
+
+        candidates = self.dict.search(tomoe_query)
+        for char, candidate in zip(sorted(set(char_codes)), sorted(self.dict.search(tomoe_query))):
+            self.assertEqual(candidate.get_char().get_utf8(), char)
+
+    def testOnReadingSearch(self):
+        char_codes = [ '垳' ]
+        reading = 'ガケ'
+        tomoe_reading = tomoe.Reading(tomoe.READING_JA_ON, reading)
+        tomoe_query = tomoe.Query()
+        tomoe_query.add_reading(tomoe_reading)
+
+        candidates = self.dict.search(tomoe_query)
+        for char, candidate in zip(sorted(set(char_codes)), sorted(self.dict.search(tomoe_query))):
+            self.assertEqual(candidate.get_char().get_utf8(), char)
+
+    def testUnknownOnReadingSearch(self):
+        char_codes = [ '垳' ]
+        reading = 'ガケ'
+        tomoe_reading = tomoe.Reading(tomoe.READING_JA_ON, reading)
+        tomoe_query = tomoe.Query()
+        tomoe_query.add_reading(tomoe_reading)
+
+        candidates = self.dict.search(tomoe_query)
+        for char, candidate in zip(sorted(set(char_codes)), sorted(self.dict.search(tomoe_query))):
+            self.assertEqual(candidate.get_char().get_utf8(), char)
 
 # vi:ts=4:nowrap:ai:expandtab
