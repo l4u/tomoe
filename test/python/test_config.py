61e5529f7f742a49d06279f922f5001240945c2a
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-13 11:36:52 +0000

    * test/python/test_config.py, test/python/test_context.py, test/python/test_writing.py: Added. All tests fail yet.

---
+++ test/python/test_config.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF=8 -*-
+import os
+import sys
+import glob
+import unittest
+import test_common
+import tomoe
+
+class TomoeConfigTest(unittest.TestCase):
+
+    def testGetFilename(self):
+        self.assert_(False)
+
+    def testGetUserDictNamer(self):
+        self.assert_(False)
+
+    def testGetLanguages(self):
+        self.assert_(False)
+
+    def testMakeShelf(self):
+        self.assert_(False)
+
+# vi:ts=4:nowrap:ai:expandtab
