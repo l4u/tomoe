ce8cc61e7a57782b42800b7ceee377e86be02e24
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-13 11:36:52 +0000

    * test/python/test_config.py, test/python/test_context.py, test/python/test_writing.py: Added. All tests fail yet.

---
+++ tomoe/trunk/test/python/test_context.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF=8 -*-
+import os
+import sys
+import glob
+import unittest
+import test_common
+import tomoe
+
+class TomoeContextTest(unittest.TestCase):
+
+    def testLoadConfig(self):
+        self.assert_(False)
+
+    def testSearch(self):
+        self.assert_(False)
+
+    def testGetChar(self):
+        self.assert_(False)
+
+    def testRegister(self):
+        self.assert_(False)
+
+    def testUnregister(self):
+        self.assert_(False)
+
+# vi:ts=4:nowrap:ai:expandtab
---
+++ tomoe/trunk/test/python/test_writing.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF=8 -*-
+import os
+import sys
+import glob
+import unittest
+import test_common
+import tomoe
+
+class TomoeWritingTest(unittest.TestCase):
+
+    def testClear(self):
+        self.assert_(False)
+
+    def testGetNStrokes(self):
+        self.assert_(False)
+
+    def testGetStrokes(self):
+        self.assert_(False)
+
+    def testRemoveLastStroke(self):
+        self.assert_(False)
+
+    def testToXML(self):
+        self.assert_(False)
+
+# vi:ts=4:nowrap:ai:expandtab
