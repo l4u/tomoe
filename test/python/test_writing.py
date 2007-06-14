99c516c6748f78b2919cd956b0ce098d60a1b1e1
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-14 03:00:47 +0000

    * test/python/test?writing.py: Implement tests.

--- test/python/test_writing.py
+++ test/python/test_writing.py
@@ -9,18 +9,72 @@ import tomoe
 class TomoeWritingTest(unittest.TestCase):
 
     def testClear(self):
-        self.assert_(False)
+        writing = tomoe.Writing()
+
+        self.assertEqual(0, len(writing.get_strokes()))
+        n = 10
+        for i in range(n):
+            writing.move_to(i, i)
+            writing.line_to(i * 2, i * 2)
+
+        writing.clear()
+        self.assertEqual(0, len(writing.get_strokes()))
 
     def testGetNStrokes(self):
-        self.assert_(False)
+        writing = tomoe.Writing()
+        self.assertEqual(0, len(writing.get_strokes()))
+
+        n = 10
+        for i in range(n):
+            writing.move_to(i, i)
+            writing.line_to(i * 2, i * 2)
+        self.assertEqual(n, writing.get_n_strokes())
 
     def testGetStrokes(self):
-        self.assert_(False)
+        writing = tomoe.Writing()
+        self.assertEqual(0, len(writing.get_strokes()))
+
+        n = 10
+        for i in range(n):
+            writing.move_to(int(i), int(i))
+            writing.line_to(int(i * 2), int(i * 2))
+        self.assertEqual(n, writing.get_n_strokes())
+       
+        for i, stroke in enumerate(writing.get_strokes()):
+            for j, point in enumerate(stroke):
+                self.assertEqual(point, (i * (j+1), i * (j+1)))
 
     def testRemoveLastStroke(self):
-        self.assert_(False)
+        writing = tomoe.Writing()
+        self.assertEqual(0, len(writing.get_strokes()))
+
+        n = 10
+        for i in range(n):
+            writing.move_to(i, i)
+            writing.line_to(i * 2, i * 2)
+        self.assertEqual(n, writing.get_n_strokes())
+        writing.remove_last_stroke()
+        
+        self.assertEqual(n - 1, writing.get_n_strokes())
+        for i, stroke in enumerate(writing.get_strokes()):
+            for j, point in enumerate(stroke):
+                self.assertEqual((i * (j+1), i * (j+1)), point)
 
     def testToXML(self):
-        self.assert_(False)
+        writing = tomoe.Writing()
+        n = 10
+        for i in range(n):
+            writing.move_to(i, i)
+            writing.line_to(i * 2, i * 2)
+
+        xml = ""
+        xml += "    <strokes>\n"
+        for i, stroke in enumerate(writing.get_strokes()):
+            xml += "      <stroke>\n"
+            for j, point in enumerate(stroke):
+                xml += "        <point x=\"%d\" y=\"%d\"/>\n" % (i * (j+1), i * (j+1))
+            xml += "      </stroke>\n"
+        xml += "    </strokes>\n"
+        self.assertEqual(xml, writing.to_xml())
 
 # vi:ts=4:nowrap:ai:expandtab
