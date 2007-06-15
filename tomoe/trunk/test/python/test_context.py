a5dbeb40206833506d2c10cbdf1918a96a4baeaa
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-15 05:48:17 +0000

    * test/python/test_context.py: Add some tests.

--- tomoe/trunk/test/python/test_context.py
+++ tomoe/trunk/test/python/test_context.py
@@ -8,14 +8,31 @@ import tomoe
 
 class TomoeContextTest(unittest.TestCase):
 
+    def setUp(self):
+        self.config_filename = "test-config"
+        config_file = open(self.config_filename, "w")
+        contents = """
+[config]
+user-dictionary = user-dict
+languages = ja;zh_CN
+         """
+        config_file.write(contents)
+        config_file.close()
+
     def testLoadConfig(self):
+        context = tomoe.Context()
+        context.load_config(self.config_filename)
         self.assert_(False)
 
     def testSearch(self):
         self.assert_(False)
 
     def testGetChar(self):
-        self.assert_(False)
+        context = tomoe.Context()
+        context.load_config(self.config_filename)
+
+        char = context.get_char('池')
+        self.assertEqual('池', char.get_utf8())
 
     def testRegister(self):
         self.assert_(False)
