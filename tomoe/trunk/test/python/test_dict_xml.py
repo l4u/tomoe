45e0eeec55d1ec72e30ed6a4e4cc56857b1bac73
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-17 07:23:08 +0000

    * test/data/test-dict.xml: Added. * test/python/test_dict_xml.py: Use test-dict.xml.

--- tomoe/trunk/test/python/test_dict_xml.py
+++ tomoe/trunk/test/python/test_dict_xml.py
@@ -1,38 +1,17 @@
 # encoding: utf-8
 import os
 import sys
+import shutil
 import unittest
+import test_common
 import tomoe
 import test_dict
 
 class TomoeDictXMLTest(test_dict.TomoeDictTest):
     def setUp(self):
+        test_dict_name = os.path.join(test_common.test_data_dir, 'test-dict.xml')
         self.dict_name = "tomoe-test-xmldict.xml"
-        dict_file = open(self.dict_name, "w")
-        contents = """
-<?xml version="1.0" standalone="no"?>
-<!DOCTYPE dictionary SYSTEM "/usr/share/tomoe/dict.dtd">
-<dictionary>
-  <character>
-    <utf8>池</utf8>
-    <number-of-strokes>6</number-of-strokes>
-    <readings>
-      <reading type="ja_on">タ</reading>
-      <reading type="ja_on">チ</reading>
-      <reading type="ja_kun">いけ</reading>
-      <reading type="ja_unknown">あんのうん</reading>
-    </readings>
-    <meta>
-      <jis208>35-51</jis208>
-      <ucs>6c60</ucs>
-      <jouyou>2</jouyou>
-      <meaning>pond, cistern, pool, reservoir</meaning>
-    </meta>
-  </character>
-</dictionary>
-         """
-        dict_file.write(contents)
-        dict_file.close()
+        shutil.copy(test_dict_name, self.dict_name)
         self.dict = tomoe.Dict("XML", filename = self.dict_name, editable = True)
 
     def tearDown(self):
