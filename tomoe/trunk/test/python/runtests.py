559fe8743f78dd8d40895e8d56726dcead3905c3
ikezoe <ikezoe@f1461e26-b952-4341-92c6-cb254f67b5cd>
2007-06-17 11:16:27 +0000

    test only building dict modules

--- tomoe/trunk/test/python/runtests.py
+++ tomoe/trunk/test/python/runtests.py
@@ -6,7 +6,8 @@ import sys
 import unittest
 import test_common
 
-SKIP_FILES = ['runtests', 'test_dict', 'test_common']
+dict_modules = os.getenv('DICT_MODULES').split()
+SKIP_FILES = ['runtests', 'test_dict', 'test_common', 'test_dict_est', 'test_dict_mysql', 'test_dict_unihan', 'test_dict_xml']
 
 dir = os.path.split(os.path.abspath(__file__))[0]
 os.chdir(dir)
@@ -14,6 +15,8 @@ os.chdir(dir)
 def gettestnames():
     files = glob.glob('*.py')
     names = map(lambda x: x[:-3], files)
+    dict_files = map(lambda x: 'test_dict_'+x , dict_modules)
+    map(SKIP_FILES.remove, dict_files) 
     map(names.remove, SKIP_FILES)
     return names
 
