b207d55f5f99d63f51ab25e11dfa00e397376918
ktou <ktou@f1461e26-b952-4341-92c6-cb254f67b5cd>
2008-05-26 03:22:33 +0000

    * data/xml2binary.c: add. (need more work (e.g. data/Makefile.am and so on...)   but maybe work.)

---
+++ data/xml2binary.c
@@ -0,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include <stdlib.h>
+#include <glib/gstdio.h>
+
+#include <tomoe.h>
+#include <tomoe-dict.h>
+
+int
+main (gint argc, gchar **argv)
+{
+    TomoeDict *xml_dict, *binary_dict;
+    gchar *binary_file_name;
+
+    if (argc != 2) {
+        g_print ("Usage: %s XXX.xml\n", argv[0]);
+        exit (EXIT_FAILURE);
+    }
+
+    tomoe_init ();
+
+    xml_dict = tomoe_dict_new ("xml",
+                               "filename" => argv[1],
+                               "editable" => FALSE,
+                               NULL);
+
+    if (g_str_has_suffix (argv[1], ".xml")) {
+        gchar *base_name;
+        base_name = g_strndup (argv[1], strlen (argv[1]) - strlen (".xml"));
+        binary_file_name = g_strconcat (base_name, ".bin", NULL);
+        g_free (base_name);
+    } else {
+        binary_file_name = g_strconcat (argv[1], ".bin", NULL);
+    }
+    binary_dict = tomoe_dict_new ("binary",
+                                  "filename" => binary_file_name,
+                                  "editable" => TRUE,
+                                  NULL);
+    g_free(binary_file_name);
+
+    tomoe_dict_copy (xml_dict, binary_dict);
+
+    g_free (xml_dict);
+    g_free (binary_dict);
+
+    tomoe_quit ();
+
+    return EXIT_SUCCESS;
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab
+*/
